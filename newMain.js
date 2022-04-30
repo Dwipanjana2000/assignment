b7e0cfa3dbd16c40ae728d19c7a00b04dd81d216
Dwipanjana2000 <103279804+Dwipanjana2000@users.noreply.github.com>
2022-04-30 06:48:27 +0530

    first commit

---
+++ newMain.js
@@ -0,0 +1,9 @@
+const headerTitle=document.getElementById('header-title');
+headerTitle.textContent='Hello';
+headerTitle.innerText='GoodBye';
+const header1=document.getElementById('main-header');
+
+header1.style.borderBottom='solid 3px #000'
+const title1=document.getElementsByClassName('title')
+console.log(title1)
+title1[0].style.fontWeight='bold'
